320c5c2d52d5e5900c31e07a98cf54380e29bf10
RakanGarvey <122347903+RakanGarvey@users.noreply.github.com>
2024-03-22 02:24:06 +0000

    fourth

--- src/Components/Roadmap.jsx
+++ src/Components/Roadmap.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Roadmap = () => {
   return (
-    <>
+    <div className='main_page'>
         <div className="level first_level"></div>
-        <div className='container'>
+        {/* <div className='container'>
             <div className='map_levels'>
                 <div className='level left_levels'><div className='line left_line'/></div>
                 <div className='level left_levels'><div className='line left_line'/></div>
@@ -14,8 +14,24 @@ const Roadmap = () => {
                 <div className='level right_levels'><div className='line right_line'/></div>
                 <div className='level right_levels'><div className='line right_line'/></div>
             </div>
+        </div> */}
+        <div className="holder right_holder">
+            <div className="arrow_right"></div>
+            <div className="level second_level"></div>
         </div>
-    </>
+        <div className="holder left_holder">
+            <div className="level third_level"></div>
+            <div className="arrow_left"></div>
+        </div>
+        <div className="holder right_holder">
+            <div className="arrow_right"></div>
+            <div className="level second_level"></div>
+        </div>
+        <div className="holder left_holder">
+            <div className="level third_level"></div>
+            <div className="arrow_left"></div>
+        </div>
+    </div>
   )
 }
 
