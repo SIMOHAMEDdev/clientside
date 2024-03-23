d0513914b041232371747332d0737e35c107bd71
RakanGarvey <122347903+RakanGarvey@users.noreply.github.com>
2024-03-23 15:15:55 +0000

    first

--- src/Components/LevelState.jsx
+++ src/Components/LevelState.jsx
@@ -1,38 +1,30 @@
 import React, { useState } from 'react'
 import Data from "../Data.json"
+import { HiPlay } from "react-icons/hi2";
 
 const LevelState = ({ active, level }) => {
-    const [width1, setWidth1] = useState("7pc")
-    const [width2, setWidth2] = useState("14pc")
-    const [width3, setWidth3] = useState("21pc")
 
-    const data = Data.level1
+    const data = Data
   return (
     <div className={active ? 'levelstate active' : 'levelstate'}>
-        <h1>Level {level}:</h1>
+        <h1>Level {level}:</h1><br />
         <section>
-            
-            {/* <label>Lesson 2:
-              <p 
-              style={{fontSize: "20px", margin: "3px 0", display: "flex", alignItems: "center", justifyContent:"space-between"}}
-              >Learning Some Words <p>2/3</p></p>
-              <div className="progress_bar"><div className="progress" style={{width: width2}}></div></div>
-            </label>
-            <label>Lesson 3:
-              <p 
-              style={{fontSize: "20px", margin: "3px 0", display: "flex", alignItems: "center", justifyContent:"space-between"}}
-              >Test Your Knowledge <p>1/3</p></p>
-              <div className="progress_bar"><div className="progress" style={{width: width1}}></div></div>
-            </label> */}
-            {/* <label>Lesson 4:</label> */}
-            {Object.keys(data).map((lesson, index)=>{
-                return (<label key={index}>{lesson}:
-                  <p 
-                  style={{fontSize: "20px", margin: "12px 0", display: "flex", alignItems: "center", justifyContent:"space-between"}}
-                  >{data[lesson].title} <span>{data[lesson].progress}</span></p>
-                  <div className="progress_bar"><div className="progress progress.animated" style={{width: data[lesson].width_progress}}></div></div>
-                </label>)
-            })}
+            {Object.keys(data).map((key)=>{
+                  console.log(parseInt(key), parseInt(level))
+                if(parseInt(key) === parseInt(level)){
+                  return Object.keys(data[key]).map((lesson, index)=>{
+                  return (<div className='fadeInDown'>
+                  <label key={index} className='lesson fadeInDown'>{lesson}: <HiPlay className='play_btn'/></label>
+                    <p className='fadeInDown'
+                    style={{fontSize: "20px", margin: "10px 0", display: "flex", alignItems: "center", justifyContent:"space-between"}}
+                    >{data[key][lesson].title} <span>{data[key][lesson].progress}</span></p>
+                    <div className="progress_bar fadeInDown"><div className="progress progress.animated" style={{width: data[key][lesson].width_progress}}></div></div>
+                    </div> )
+                })}else{
+                  return null
+                }
+                // : null
+              })}
         </section>
     </div>
   )
