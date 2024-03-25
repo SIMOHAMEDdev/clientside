149c083db79cd9a09c8ceb5f223b9a732ba14324
RakanGarvey <122347903+RakanGarvey@users.noreply.github.com>
2024-03-25 11:55:07 +0000

    first

--- src/Components/LevelState.jsx
+++ src/Components/LevelState.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react'
-import Data from "../Data.json"
+import Arabic from "../Arabic.json"
 import { HiPlay } from "react-icons/hi2";
 import { useNavigate } from 'react-router-dom';
 
 const LevelState = ({ active, level }) => {
   const navigate = useNavigate()
 
-    const data = Data
+    const data = Arabic
   return (
     <div className={active ? 'levelstate active' : 'levelstate'}>
-        <h1>Level {level}:</h1><br />
+        <h1>:المستوى {level}</h1><br />
         <section>
             {Object.keys(data).map((key)=>{
-                if(parseInt(key) === parseInt(level)){
+                if(key === level){
                   return Object.keys(data[key]).map((lesson, index)=>{
                   return (<div className='fadeInDown'  key={index}>
-                  <label className='lesson fadeInDown'>{lesson}: <HiPlay onClick={()=>{navigate("/roadmap/learning")}} className='play_btn'/></label>
+                  <label className='lesson fadeInDown'>{lesson} <HiPlay onClick={()=>{navigate("/roadmap/learning")}} className='play_btn'/></label>
                     <p className='fadeInDown'
                     style={{fontSize: "20px", margin: "10px 0", display: "flex", alignItems: "center", justifyContent:"space-between"}}
                     >{data[key][lesson].title} <span>{data[key][lesson].progress}</span></p>
--- src/Components/Roadmap.jsx
+++ src/Components/Roadmap.jsx
@@ -2,7 +2,7 @@ import { Children, useState } from 'react';
 import LevelState from '../Components/LevelState'
 
 const Roadmap = () => {
-    const [levels, setLevels] = useState([2, 3, 4, 5])
+    const [levels, setLevels] = useState([2, 3])
     const [active, setActive] = useState(false)
     const [level, setLevel] = useState(0)
 
@@ -11,9 +11,9 @@ const Roadmap = () => {
         <div className='roadmap'>
             <div className="level first_level" onClick={(e)=>{setActive(true)
             setLevel(e.currentTarget.innerText)}}>
-                <p>1</p>
+                <p>١</p>
             </div>
-            {levels.map((num, index) => 
+            {/* {levels.map((num, index) => 
                 num % 2 === 0 ?
                     <div className="holder right_holder" key={index}>
                         <div className="arrow_right"></div>
@@ -30,7 +30,21 @@ const Roadmap = () => {
                         </div>
                         <div className="arrow_left"></div>
                     </div>
-            )}
+            )} */}
+                    <div className="holder right_holder" >
+                        <div className="arrow_right"></div>
+                        <div className="level second_level" onClick={(e)=>{setActive(true)
+                        setLevel(e.currentTarget.innerText)}}>
+                            <p>٢</p>
+                        </div>
+                    </div> 
+                    <div className="holder left_holder"> 
+                        <div className="level third_level" onClick={(e)=>{setActive(true)
+                        setLevel(e.currentTarget.innerText)}}>
+                        <p>٣</p>
+                        </div>
+                        <div className="arrow_left"></div>
+                    </div>
         </div>
         <LevelState active={active} level={level}/>
     </div>
