d20426f945f0e298a3b5b4e85aede99cf66d7fa9
Abdelrahman <abdelrahman.elsaudy@gmail.com>
2024-03-29 05:46:27 +0200

    modifying moves data

--- src/data/data.py
+++ src/data/data.py
@@ -4,5 +4,10 @@ import json
 numbers_data = pandas.read_csv("numbers.csv")
 numbers_dicts = numbers_data.to_dict(orient="records")
 
+moves_data = pandas.read_csv("moves.csv")
+moves_dicts = moves_data.to_dict(orient="records")
+
+print(moves_dicts)
+
 with open("numbers.json", "w") as json_file:
     json.dump(numbers_dicts, json_file)
