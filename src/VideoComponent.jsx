84f641ec6b4fa4c83e8d535745450bedcdf190e4
Simo Hamed <122347903+SIMOHAMEDdev@users.noreply.github.com>
2024-03-30 19:01:18 +0000

    FINAL COMMIT

--- src/VideoComponent.jsx
+++ src/VideoComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useeffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const Video = ({video}) => {
   const [playing, setPlaying] = useState(false);
